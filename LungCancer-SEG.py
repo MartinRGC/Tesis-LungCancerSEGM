2a67c151638489b2647d283174ba386b14721d2c
MartinRGC <104601408+MartinRGC@users.noreply.github.com>
2024-12-09 12:47:56 -0500

    unzip files

--- LungCancer-SEG.py
+++ LungCancer-SEG.py
@@ -4,6 +4,16 @@
 # pip install kaggle
 # kaggle competitions download -c vinbigdata-chest-xray-abnormalities-detection
 #
-import os
-os.environ['KAGGLE_CONFIG_DIR'] = os.getcwd()
-os.system('kaggle competitions download -c vinbigdata-chest-xray-abnormalities-detection')
+#import os
+#import zipfile
+#os.environ['KAGGLE_CONFIG_DIR'] = os.getcwd()
+#os.system('kaggle competitions download -c vinbigdata-chest-xray-abnormalities-detection')
+#zip_file_path = 'vinbigdata-chest-xray-abnormalities-detection.zip'
+#extract_dir = 'vinbigdata_chest_xray'
+
+#os.makedirs(extract_dir, exist_ok=True)
+
+#with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+#    zip_ref.extractall(extract_dir)
+
+#print(f"Archivos descomprimidos en {extract_dir}")
